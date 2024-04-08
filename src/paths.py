c0f2177c3c2f5b89f375653a452d9173d9e5d364
VladislavSpirydonau <162104721+VladislavSpirydonau@users.noreply.github.com>
2024-04-08 23:11:59 +0200

    debuging

--- src/paths.py
+++ src/paths.py
@@ -1,9 +1,11 @@
 import os
 parent_dir = os.path.dirname(os.path.dirname(__file__))
+
 csv_filename = "client_ids.csv"
 client_csv_path = os.path.join(parent_dir,'data', csv_filename)
+
 db_filename = 'data.db'
 db_path = os.path.join(parent_dir, 'data', db_filename)
 
 age_pipeline_file = 'age_pipeline.pkl'
-db_path = os.path.join(parent_dir, 'pipelines', age_pipeline_file)
+age_pipeline_path = os.path.join(parent_dir, 'pipelines', age_pipeline_file)
--- src/sql_querys.py
+++ src/sql_querys.py
@@ -19,13 +19,10 @@ on client.client_id = mean_deposite.client_id''')
 
 
 query_training_model = ('''
-WITH mean_deposite as (
+WITH max_deposite as (
     select 
          client_id,
-         AVG(balance) as mean_balance,
          MAX(balance) as max_balance,
-         MIN(balance) as min_balance,
-         MAX(balance)-MIN(balance) as dif_balance,
          currency
     from balances
     group by 1
@@ -36,5 +33,11 @@ left join client
 on inv_campaign_eval.client_id = client.client_id
 left join client_products
 on inv_campaign_eval.client_id = client_products.client_id
-left join mean_deposite
-on inv_campaign_eval.client_id = mean_deposite.client_id''')
+left join max_deposite
+on inv_campaign_eval.client_id = max_deposite.client_id''')
+
+query_square_sum = ('''
+Select * 
+from inv_campaign_eval
+left join balances
+on inv_campaign_eval.client_id = balances.client_id''')
