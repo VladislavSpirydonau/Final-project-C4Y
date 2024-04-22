500029b4a9947d4245a3e3928e4da72884cf9152
VladislavSpirydonau <162104721+VladislavSpirydonau@users.noreply.github.com>
2024-04-22 20:59:38 +0200

    dividing pipeline to separate script

--- final_model.py
+++ final_model.py
@@ -1,13 +1,8 @@
 import sqlite3
 import pandas as pd
-from src import paths,sql_querys,converting
+from src import paths,sql_querys,converting,main_pipeline
 import joblib
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, accuracy_score, recall_score
-from sklearn.preprocessing import StandardScaler, OrdinalEncoder, LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
 # 1. Creating a dataset from database and cleaning the data
@@ -18,14 +13,17 @@ df = pd.read_sql_query(query, conn)
 df = df.loc[:,~df.columns.duplicated()]
 df.max_balance = pd.to_numeric(df.max_balance)
 df.age=pd.to_numeric(df.age)
+
 # 2. Converting balances to CZK
 rate = {'CZK': 1, 'USD': 23, 'EUR': 25}
 df = converting.converter(df, rate)
+
 # 3. Filling missing values
 age_pipeline = joblib.load(paths.age_pipeline_path)
 missing_age_indices = df['age'].isnull()
 predicted_ages = age_pipeline.predict(df[missing_age_indices])
 df.loc[missing_age_indices, 'age'] = predicted_ages
+
 # 4. Creating new feature 
 query1 = (sql_querys.query_square_sum)
 dif_bal = pd.read_sql_query(query1, conn)
@@ -35,35 +33,22 @@ dif_bal['balance_diff'] = dif_bal.groupby('client_id')['balance'].diff().fillna(
 dif_bal['balance_diff_square_sum'] = dif_bal.groupby('client_id')['balance_diff'].transform(lambda x: (x ** 2).sum())
 dif_bal.drop(columns=['date', 'balance', 'balance_diff', 'poutcome', 'currency'], inplace=True)
 df = df.merge(dif_bal.drop_duplicates(subset=['client_id']), on='client_id', how='left')
+
 # 5. Creating training data
 y = df.poutcome
 X = df.drop(columns = 'poutcome')
 enc = LabelEncoder()
 y=enc.fit_transform(y)
-# 6. Pipeline
+
+# 6.Pipeine
+# 6.1 Preparing parametrs for pipeline function
 num_features = ['age', 'balance_diff_square_sum', 'max_balance']
 labeled_features = ['has_deposits', 'loan', 'has_mortgage','education' ]
+RFC_params = {'random_state':42, 'n_estimators': 150, 'min_samples_split': 10,
+                                            'min_samples_leaf':2, 'max_features': 'sqrt', 'max_depth': 10, 'criterion':'entropy', 'bootstrap': False}
+# 6.2 calling the pipeline function
+pipeline = main_pipeline.main_pipeline(num_features,labeled_features,RFC_params)
 
-# 6.1. Create transformers for the numerical and categorical features
-numeric_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='median')),
-    ('scaler', StandardScaler())])
-
-labeled_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='constant', fill_value='no')),
-    ('label', OrdinalEncoder())])
-
-# 6.2. Create a column transformer to apply the transformations to the respective columns
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', numeric_transformer, num_features),
-        ('label', labeled_transformer, labeled_features)
-            ])
-
-# 6.3. Full pipeline
-pipeline = Pipeline(steps=[
-    ('preprocessor', preprocessor),
-    ('classifier', RandomForestClassifier(random_state=42, n_estimators= 150, min_samples_split= 10, min_samples_leaf= 2, max_features= 'sqrt', max_depth= 10, criterion= 'entropy', bootstrap= False))])
 # 7. Training and saving the pipeline
 #7.1. I test the model before and now how it perform so for actualy training the model for further predictions I can use every data I have
 pipeline.fit(X, y)
